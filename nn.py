5a96f31d7a2c95cc1d4a9029f9aaa973fb695c14
ParthKalkar <ParthKalkar111@gmail.com>
2022-09-01 11:24:35 +0300

    created-matrix

---
+++ nn.py
@@ -0,0 +1,8 @@
+class NeuralNetwork: 
+    def __init__(self, input_nodes, hidden_nodes, output_nodes):
+        self.input_nodes = input_nodes
+        self.hidden_nodes = hidden_nodes
+        self.output_nodes = output_nodes
+
+    def feedfoward(input): 
+        #todo
